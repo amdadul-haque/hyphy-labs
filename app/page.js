282c3ba0db8e45fce725a8cf16859685b08fe510
amdadul-haque <amdadul1807107@gmail.com>
2023-12-28 03:11:09 +0600

    font updated

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>Main Page yeah</h1>
+      <h1 className='text-center text-5xl font-primary'>Main Page yeah</h1>
     </main>
   )
 }
